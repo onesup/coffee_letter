d5f8a6353e8972f85469049f75e938996d20ac0d
Wonsup Lee <daul0212@gmail.com>
2013-08-03 20:46:39 +0900

    login form

---
+++ app/controllers/admin/letters_controller.rb
@@ -0,0 +1,85 @@
+class Admin::LettersController < ApplicationController
+  def index
+    @letters = Letter.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @letters }
+    end
+  end
+
+  # GET /letters/1
+  # GET /letters/1.json
+  def show
+    @letter = Letter.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @letter }
+    end
+  end
+
+  # GET /letters/new
+  # GET /letters/new.json
+  def new
+    @letter = Letter.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @letter }
+    end
+  end
+
+  # GET /letters/1/edit
+  def edit
+    @letter = Letter.find(params[:id])
+  end
+
+  # POST /letters
+  # POST /letters.json
+  def create
+    @letter = Letter.new(params[:letter])    
+    respond_to do |format|
+      if @letter.save
+        @letter.billings.create()
+        if @letter.controller_name == 'main'
+          format.html { redirect_to edit_letter_path(@letter) }
+        else
+          format.html { redirect_to @letter, notice: 'Letter was successfully created.' }
+          format.json { render json: @letter, status: :created, location: @letter }
+        end
+      else
+        format.html { render action: "new" }
+        format.json { render json: @letter.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /letters/1
+  # PUT /letters/1.json
+  def update
+    @letter = Letter.find(params[:id])
+
+    respond_to do |format|
+      if @letter.update_attributes(params[:letter])
+        format.html { redirect_to @letter, notice: 'Letter was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @letter.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /letters/1
+  # DELETE /letters/1.json
+  def destroy
+    @letter = Letter.find(params[:id])
+    @letter.destroy
+
+    respond_to do |format|
+      format.html { redirect_to letters_url }
+      format.json { head :no_content }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 CoffeeLetter::Application.routes.draw do
+
+  get "logout"  => "sessions#destroy", :as => "logout"
+  get "login"   => "sessions#new", :as => "login"
+
+  resources :users, :only => [:create]
+  resources :sessions
+  resources :letters, except: [:destroy, :index]
   resources :billings
-
-
+  namespace :admin do
+    resources :letters
+  end
+  
   get "main/index"
-
-  resources :letters
   root :to => 'main#index'
-
 end
