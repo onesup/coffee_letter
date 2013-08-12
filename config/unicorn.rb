3d622b2905abee86ce32cbea379ae73f45af52eb
Wonsup Lee <daul0212@gmail.com>
2013-08-13 05:40:25 +0900

    config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,28 +1,21 @@
-# root = "/home/daul/coffee_letter/current"
-# working_directory root
-# pid "#{root}/tmp/pids/unicorn.pid"
-# stderr_path "#{root}/log/unicorn.err.log"
-# stdout_path "#{root}/log/unicorn.out.log"
-#  
-# # change the YOUR_APP_NAME to your application name
-# listen "/tmp/unicorn.coffee_letter.sock"
-# worker_processes 2
-# timeout 30
-# 
-# before_exec do |server|
-#   ENV["BUNDLE_GEMFILE"] = "#{root}/Gemfile"
-# end
-# 
-# 
-
 root = "/home/daul/coffee_letter/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
-
-worker_processes 3
-timeout 30
-preload_app true
+stderr_path "#{root}/log/unicorn.err.log"
+stdout_path "#{root}/log/unicorn.out.log"
+ 
+# change the YOUR_APP_NAME to your application name
 listen "/tmp/unicorn.coffee_letter.sock"
+worker_processes 2
+timeout 30
+preload_app true# 
+before_exec do |server|
+  ENV["BUNDLE_GEMFILE"] = "#{root}/Gemfile"
+end
+
+
+
+
 before_fork do |server, worker|
 
   Signal.trap 'TERM' do
