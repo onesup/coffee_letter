3ad706aef8e64061eac6eba2591a6e5f5fcb8873
Wonsup Lee <daul0212@gmail.com>
2013-08-13 15:47:12 +0900

    회원가입 에러 수
    permit 지정 안함.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 #encoding: utf-8
 class UsersController < ApplicationController
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       auto_login(@user)
       redirect_to login_path, :notice => @user.email+ "님 가입 감사드립니다!"
@@ -12,4 +12,10 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  private  
+  
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
 end
