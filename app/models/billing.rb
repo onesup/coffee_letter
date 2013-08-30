d9aefaef123b7026bc952dfb1e6bb21290b6da4e
Wonsup Lee <daul0212@gmail.com>
2013-08-30 11:57:35 +0900

    편지 내용 보기에 보낸이 전화번호 추가

--- app/models/billing.rb
+++ app/models/billing.rb
@@ -4,17 +4,15 @@ class Billing < ActiveRecord::Base
   belongs_to :user
   accepts_nested_attributes_for :letters
   
-  # before_create :create_or_find_default_product
   before_create :billing_has_default_product
   
   private
     def create_or_find_default_product
       if Product.exists?(title: "default_product")
-        p = Product.where(title: "default_product").first
+        Product.where(title: "default_product").first
       else
-        p = Product.create(title: "default_product", price: 3400, discount_value: 0.1)
+        Product.create(title: "default_product", price: 3400, discount_value: 0.1)
       end
-      return p
     end
     
     def billing_has_default_product
